519b8661c4a344812479c2839cb04a68283979e6
dvanzadelhoff <dvanzadelhoff2412@gmail.com>
2022-06-11 00:35:16 +0200

    Keep Unit Interfaces Small

---
+++ src/StoelInfo.java
@@ -0,0 +1,15 @@
+public class StoelInfo
+{
+    private String stoelLabel;
+    private String stoelType;
+
+
+
+    public String getStoelType() {
+        return stoelType;
+    }
+
+    public String getStoelLabel() {
+        return stoelLabel;
+    }
+}
